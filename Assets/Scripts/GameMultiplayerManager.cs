0260ed9ee88093c7fc1fa244e8c04ad7e05c0a8f
ratala <talaia.gamedev@pm.me>
2024-02-18 12:38:20 -0500

    Ajout sélection naïve du personnage
    Ajout sans mesage d'erreur et
    sans prise en compte état prêt des joueurs.

--- Assets/Scripts/GameMultiplayerManager.cs
+++ Assets/Scripts/GameMultiplayerManager.cs
@@ -10,15 +10,22 @@ public class GameMultiplayerManager : NetworkBehaviour
     
     public static int MAX_NUMBER_OF_PLAYERS = 2;
 
+    private NetworkList<PlayerData> _playerDataNetworkList;
+
     private void Awake()
     {
         Instance = this;
+
+        _playerDataNetworkList = new NetworkList<PlayerData>();
+        _playerDataNetworkList.OnListChanged += PlayerDataNetworkList_OnListChanged;
         
         DontDestroyOnLoad(gameObject);
     }
 
     public void StartHost()
     {
+        NetworkManager.Singleton.OnClientConnectedCallback += NetworkManager_Host_OnClientConnectedCallback;
+        NetworkManager.Singleton.OnClientDisconnectCallback += NetworkManager_Host_OnClientDisconnectCallback;
         NetworkManager.Singleton.StartHost();
     }
 
@@ -26,4 +33,59 @@ public class GameMultiplayerManager : NetworkBehaviour
     {
         NetworkManager.Singleton.StartClient();
     }
+
+    public void SelectCharacterVisual(CharacterSelectUI.CharacterId characterId)
+    {
+        SelectCharacterServerRpc(characterId);
+    }
+    
+    [ServerRpc(RequireOwnership = false)]
+    private void SelectCharacterServerRpc
+        (CharacterSelectUI.CharacterId characterId, ServerRpcParams serverRpcParams = default)
+    {
+        // change _playerDataNetworkList for characterVisualId
+    }
+    
+    /**
+     * Returns host, as Item1, and client, as Item2, character selection
+     */
+    public (CharacterSelectUI.CharacterId, CharacterSelectUI.CharacterId) GetCharacterVisualSelection()
+    {
+        if (_playerDataNetworkList.Count == MAX_NUMBER_OF_PLAYERS)
+        {
+            return (_playerDataNetworkList[0].characterSelection, _playerDataNetworkList[1].characterSelection);
+        }
+        else
+        {
+            return (_playerDataNetworkList[0].characterSelection, CharacterSelectUI.CharacterId.None);
+        }
+    }
+    
+    public event EventHandler OnPlayerDataNetworkListChanged;
+    
+    private void PlayerDataNetworkList_OnListChanged(NetworkListEvent<PlayerData> changeEvent)
+    {
+        OnPlayerDataNetworkListChanged?.Invoke(this, EventArgs.Empty);
+    }
+    
+    private void NetworkManager_Host_OnClientConnectedCallback(ulong clientId)
+    {
+        _playerDataNetworkList.Add(new PlayerData
+        {
+            clientId = clientId,
+            characterSelection = CharacterSelectUI.CharacterId.First
+        });
+    }
+
+    private void NetworkManager_Host_OnClientDisconnectCallback(ulong clientId)
+    {
+        foreach (PlayerData playerData in _playerDataNetworkList)
+        {
+            if (playerData.clientId == clientId)
+            {
+                _playerDataNetworkList.Remove(playerData);
+            }
+        }
+    }
+
 }
---
+++ Assets/Scripts/UI/CharacterSelectUI.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UI;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterSelectUI : MonoBehaviour
+{
+    [SerializeField] private Button selectFistCharacterButton;
+    [SerializeField] private Button selectSecondCharacterButton;
+    
+    // La marque est affichee lorsque le joueur selectionne le personnage correspondant.
+    [SerializeField] private Image hostFirstCharacterMark;
+    [SerializeField] private Image clientFirstCharacterMark;
+    
+    [SerializeField] private Image hostSecondCharacterMark;
+    [SerializeField] private Image clientSecondCharacterMark;
+    
+    public enum CharacterId
+    {
+        First = 0,
+        Second = 1,
+        None = 2,
+    }
+
+    private void Awake()
+    {
+        selectFistCharacterButton.onClick.AddListener(() =>
+        {
+            GameMultiplayerManager.Instance.SelectCharacterVisual(CharacterId.First);
+        });
+        
+        selectSecondCharacterButton.onClick.AddListener(() =>
+        {
+            GameMultiplayerManager.Instance.SelectCharacterVisual(CharacterId.Second);
+        });
+    }
+
+    private void Start()
+    {
+        GameMultiplayerManager.Instance.OnPlayerDataNetworkListChanged += GameMultiplayerManager_OnPlayerDataNetworkListChanged;
+    }
+
+    private void GameMultiplayerManager_OnPlayerDataNetworkListChanged(object sender, EventArgs e)
+    {
+        UpdateCharacterMarkVisuals();
+    }
+
+    private void UpdateCharacterMarkVisuals()
+    {
+        (CharacterId, CharacterId) playersSelection = GameMultiplayerManager.Instance.GetCharacterVisualSelection();
+        
+        SetHostMarkActive(playersSelection.Item1);
+        
+        SetClientMarkActive(playersSelection.Item2);
+    }
+
+    private void SetHostMarkActive(CharacterId selectedId)
+    {
+        SetMarkActive(hostFirstCharacterMark, hostSecondCharacterMark, selectedId);
+    }
+    
+    private void SetClientMarkActive(CharacterId selectedId)
+    {
+        SetMarkActive(clientFirstCharacterMark, clientSecondCharacterMark, selectedId);
+    }
+
+    private void SetMarkActive(Image fisrtCharacterMark, Image secondCharacterMark, CharacterId selectedId)
+    {
+        if (selectedId == CharacterId.First)
+        {
+            BasicShowHide.Show(fisrtCharacterMark.gameObject);
+            BasicShowHide.Hide(secondCharacterMark.gameObject);
+        }
+
+        if (selectedId == CharacterId.Second)
+        {
+            BasicShowHide.Hide(fisrtCharacterMark.gameObject);
+            BasicShowHide.Show(secondCharacterMark.gameObject);
+        }
+
+        if (selectedId == CharacterId.None)
+        {
+            BasicShowHide.Hide(fisrtCharacterMark.gameObject);
+            BasicShowHide.Hide(secondCharacterMark.gameObject);
+        }
+        
+    }
+
+    private void OnDestroy()
+    {
+        GameMultiplayerManager.Instance.OnPlayerDataNetworkListChanged -= GameMultiplayerManager_OnPlayerDataNetworkListChanged;
+    }
+}
