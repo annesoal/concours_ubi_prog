cbd2d873ffc16eb11301f5a71f61d61f18df351d
nbeeckmans <nicolas.beeckmans@hotmail.com>
2024-02-19 14:43:26 -0500

    Implementer premier iteration mouvement joueur
    - TODO : separer le selector du joueur

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,31 +1,30 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using DefaultNamespace;
-using Unity.Collections.LowLevel.Unsafe;
-using Unity.Mathematics;
 using Unity.Netcode;
-using UnityEditor.Profiling;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 public class Player : NetworkBehaviour
 {
-
     [SerializeField] private GameObject playerVisuals;
-    [Header("Player Movement")]
-    [SerializeField] private float playerSpeed;
+
+    [Header("Player Movement")] [SerializeField]
+    private float playerSpeed;
+
     [SerializeField] private float playerGravity;
     [SerializeField] private float playerJumpForce;
     [SerializeField] private TileSelector tileSelector;
-    private Rigidbody _playerRigidBody;
+
+    public bool CanSelectectNextTile
+    {
+        set => _canSelectNextTile = value;
+    }
+    
+    private bool _canSelectNextTile = true;
     private Collider _playerCollider;
-    private bool _canSelectNextTile = true; 
+    private Rigidbody _playerRigidBody;
 
     private void Awake()
     {
-         _playerRigidBody = GetComponent<Rigidbody>();
-         _playerCollider = playerVisuals.GetComponent<CapsuleCollider>();
+        _playerRigidBody = GetComponent<Rigidbody>();
+        _playerCollider = playerVisuals.GetComponent<CapsuleCollider>();
     }
 
     private void Start()
@@ -36,46 +35,12 @@ public class Player : NetworkBehaviour
     private void Update()
     {
         if (_canSelectNextTile)
-        {
+            // TODO : Utiliser autre systeme d'input
             if (Input.GetKey(KeyCode.Space))
             {
                 _canSelectNextTile = false;
                 tileSelector.Show();
                 tileSelector.CanMove = true;
             }
-        }
-        //  HandleMovement();
-    }
-    
-    private void HandleMovement()
-    {
-        Vector2 movementNormalized = GameInput.Instance.GetMovementNormalized();
-
-        Vector3 movementToApply = new Vector3(movementNormalized.x, 0, movementNormalized.y);
-
-        transform.position += movementToApply * (Time.deltaTime * playerSpeed);
-
-        if (Input.GetKey(KeyCode.Space) && IsGrounded())
-        {
-            Vector3 vector3 = _playerRigidBody.velocity;
-            vector3.y = playerJumpForce;
-            _playerRigidBody.velocity = vector3;
-        }
-
-        ApplyPlayerGravity();
-    }
-
-    private bool IsGrounded()
-    {
-        Vector3 position = transform.position;
-        
-        Debug.Log("isGrounded : " + Physics.Raycast(position, new Vector3(0, -1, 0), 0.1f));
-        
-        return Physics.Raycast(_playerCollider.bounds.center, Vector3.down, 1.5f);
-    }
-
-    private void ApplyPlayerGravity()
-    {
-        _playerRigidBody.AddForce(0, playerGravity, 0);
     }
 }
--- Assets/Scripts/TileSelector.cs
+++ Assets/Scripts/TileSelector.cs
@@ -1,62 +1,71 @@
 using UnityEngine;
-using UnityEngine.Serialization;
-using UnityEngine.InputSystem; 
+using UnityEngine.InputSystem;
 
-namespace DefaultNamespace
+public class TileSelector : MonoBehaviour
 {
-    public class TileSelector : MonoBehaviour
+    private const float Cooldown = 0.1f;
+    [SerializeField] private GameObject quad;
+    [SerializeField] private Player player;
+    public InputAction move;
+    private bool _canMove;
+    private float _cooldown;
+    
+
+    public bool CanMove
     {
-        [SerializeField] private GameObject quad;
-        private bool _canMove;
-        private const float Cooldown = 0.1f; 
-        private float _cooldown = 0.0f;  
-        public InputAction move;
-        public bool CanMove
-        {
-            set
-            {
-                _canMove = value;
-            }
-        } 
-        void Start()
-        {
-            move.Enable();
-        }
-        //Permet de deplacer le Selector... TODO : A changer car trop dur atm !  
-        public void Move()
-        {
-            Vector2 input = move.ReadValue<Vector2>();
-            // x de input, y de input == x,z en 3d
-            Vector3 directionToAdd = new Vector3(input.x, 0  ,input.y);
+        set => _canMove = value;
+    }
 
-            if (_cooldown <= 0.0f)
-            {
-                transform.position += directionToAdd;
-                _cooldown = Cooldown;
-            }
-            else
-            {
-                // reduit le cd par le temps entre chaque frame 
-                _cooldown -= Time.deltaTime; 
-            }
-        }
+    private void Start()
+    {
+        move.Enable();
+    }
 
-        void Update()
-        {
-            if (_canMove)
-            {
-                Move();
-            }
-        }
+    private void Update()
+    {
+        if (_canMove) Move();
+        // TODO : utiliser le nouveau system ! 
+        if (Input.GetKey(KeyCode.Space)) MovePlayer(); 
+    }
 
-        public void Hide()
-        {
-            quad.SetActive(false);
-        }
+    //Permet de deplacer le Selector... TODO : A changer car trop saccade!  
+    public void Move()
+    {
+        var input = move.ReadValue<Vector2>();
+        // x de input, y de input == x,z en 3d
+        var directionToAdd = new Vector3(input.x, 0, input.y);
 
-        public void Show()
+        if (_cooldown <= 0.0f)
         {
-            quad.SetActive(true);
+            transform.position += directionToAdd;
+            _cooldown = Cooldown;
         }
+        else
+        {
+            // reduit le cd par le temps entre chaque frame 
+            _cooldown -= Time.deltaTime;
+        }
+    }
+
+    // prablement a diviser en sous methode ? 
+    // deplace le joueur la ou le selector se trouve, empeche le selector de bouger, cache le selector et indique 
+    // au joueur qu'il peut recommencer le processus de selection
+    public void MovePlayer()
+    {
+        _canMove = false;
+        player.transform.position = 
+            new Vector3(transform.position.x, player.transform.position.y, transform.position.z);
+        player.CanSelectectNextTile = true;
+        Hide();
+    }
+
+    public void Hide()
+    {
+        quad.SetActive(false);
+    }
+
+    public void Show()
+    {
+        quad.SetActive(true);
     }
 }
