bbfa9366f53ae77e30d0352c2cc5c061e6ee0f33
ratala <talaia.gamedev@pm.me>
2024-02-27 09:14:12 -0500

    Ajustement ResetStaticData sélection joueur UI

--- Assets/Scripts/UI/CharacterSelectUI.cs
+++ Assets/Scripts/UI/CharacterSelectUI.cs
@@ -98,7 +98,7 @@ public class CharacterSelectUI : MonoBehaviour
         GameMultiplayerManager.Instance.OnPlayerDataNetworkListChanged -= GameMultiplayerManager_OnPlayerDataNetworkListChanged;
     }
 
-    public void ResetStaticData()
+    public static void ResetStaticData()
     {
         OnAnyCharacterSelectChanged = null;
     }
