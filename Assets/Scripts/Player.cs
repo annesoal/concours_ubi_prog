04a4d82117a6e519b46cdf594391246c5de6dc87
Nicolas Beeckmans <beeckmans.nicolas@ens.uqam.ca>
2024-02-29 21:28:41 -0500

    Ajouter timer pour ralentir la vitesse des inputs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,5 +1,6 @@
 using System;
 using Grid.Blocks;
+using Unity.Multiplayer.Samples.Utilities.ClientAuthority.Utils;
 using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -16,10 +17,13 @@ public class Player : NetworkBehaviour
     private const string SPAWN_POINT_COMPONENT_ERROR =
         "Chaque spawn point de joueur doit avoir le component `BlockPlayerSpawn`";
 
-    void Awake()
+    private Timer _timer;
+
+    public Player()
     {
-        
+        _timer = new(cooldown);
     }
+    
     public override void OnNetworkSpawn()
     {
         CharacterSelectUI.CharacterId characterSelection =
@@ -49,31 +53,48 @@ public class Player : NetworkBehaviour
     }
     public void OnMovement(InputValue value)
     {
+        //TODO : RAAAAAAPPPHHH ?? CA MARCHE ?? 
         if (!IsOwner) return;
-        if (!IsMovingSelector) return;  
+        if (!IsMovingSelector) return;
+        if (!CanMove()) return;
+        
+        Vector2Int input = Translate(value);
+        _selector.MoveSelector(input);
+        _timer.Start();
+    }
+
+    private bool CanMove()
+    {
+        return _timer.HasTimePassed();
+    }
+    
+    // Traduite la valeur d'input en Vector2Int
+    private Vector2Int Translate(InputValue value)
+    {
         var input = value.Get<Vector2>();
         Vector2Int translation = new Vector2Int();
-
+        // pas tres jolie mais franchement ca marche 
         if (input.x > MinPressure)
         {
-            translation.x = 1; 
+            translation.x = 1;
         }
 
         if (input.x < -MinPressure)
         {
-            translation.x = -1; 
+            translation.x = -1;
         }
 
         if (input.y > MinPressure)
         {
-            translation.y = +1; 
+            translation.y = +1;
         }
 
         if (input.y < -MinPressure)
         {
             translation.y = -1;
         }
-        _selector.MoveSelector(translation);
+
+        return translation;
     }
 
     public void OnSelect()
@@ -90,4 +111,5 @@ public class Player : NetworkBehaviour
             IsMovingSelector = true; 
         }
     }
+    
 }
