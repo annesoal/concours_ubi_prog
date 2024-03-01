525d04c3c367ad107a571ac93da8ced306b53c20
Nicolas Beeckmans <beeckmans.nicolas@ens.uqam.ca>
2024-03-01 12:52:51 -0500

    Mettre le deplacement dans Update

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using Grid.Blocks;
 using Unity.Multiplayer.Samples.Utilities.ClientAuthority.Utils;
 using Unity.Netcode;
@@ -8,22 +9,39 @@ using UnityEngine.InputSystem;
 public class Player : NetworkBehaviour
 {
     private const float MinPressure = 0.3f; 
-    [SerializeField] private float cooldown = 0.2f;
-    
-     private bool IsMovingSelector { get; set; }
-
-    [SerializeField] private TileSelector _selector;
-    
     private const string SPAWN_POINT_COMPONENT_ERROR =
         "Chaque spawn point de joueur doit avoir le component `BlockPlayerSpawn`";
+    [SerializeField] private float cooldown = 0.2f;
+    [SerializeField] private TileSelector _selector;
 
+    private PlayerInputActions _playerInputActions;
+    private bool IsMovingSelector { get; set; }
     private Timer _timer;
 
     public Player()
     {
         _timer = new(cooldown);
     }
-    
+
+    void Awake()
+    {
+        _playerInputActions = new PlayerInputActions();
+        _playerInputActions.Player.Enable();
+        _playerInputActions.Player.Select.performed += OnSelect;
+    }
+
+    void Update()
+    {
+        Vector2 value = _playerInputActions.Player.Movement.ReadValue<Vector2>();
+        //TODO : RAAAAAAPPPHHH ?? CA MARCHE ?? 
+        if (!IsOwner) return;
+        if (!IsMovingSelector) return;
+        if (!CanMove()) return;
+        Vector2Int input = Translate(value);
+        _selector.MoveSelector(input);
+        _timer.Start();     
+       
+    }
     public override void OnNetworkSpawn()
     {
         CharacterSelectUI.CharacterId characterSelection =
@@ -51,45 +69,32 @@ public class Player : NetworkBehaviour
             Debug.LogError(SPAWN_POINT_COMPONENT_ERROR);
         }
     }
-    public void OnMovement(InputValue value)
-    {
-        //TODO : RAAAAAAPPPHHH ?? CA MARCHE ?? 
-        if (!IsOwner) return;
-        if (!IsMovingSelector) return;
-        if (!CanMove()) return;
-        
-        Vector2Int input = Translate(value);
-        _selector.MoveSelector(input);
-        _timer.Start();
-    }
-
     private bool CanMove()
     {
         return _timer.HasTimePassed();
     }
     
     // Traduite la valeur d'input en Vector2Int
-    private Vector2Int Translate(InputValue value)
+    private Vector2Int Translate(Vector2 value)
     {
-        var input = value.Get<Vector2>();
         Vector2Int translation = new Vector2Int();
         // pas tres jolie mais franchement ca marche 
-        if (input.x > MinPressure)
+        if (value.x > MinPressure)
         {
             translation.x = 1;
         }
 
-        if (input.x < -MinPressure)
+        if (value.x < -MinPressure)
         {
             translation.x = -1;
         }
 
-        if (input.y > MinPressure)
+        if (value.y > MinPressure)
         {
             translation.y = +1;
         }
 
-        if (input.y < -MinPressure)
+        if (value.y < -MinPressure)
         {
             translation.y = -1;
         }
@@ -97,7 +102,7 @@ public class Player : NetworkBehaviour
         return translation;
     }
 
-    public void OnSelect()
+    public void OnSelect(InputAction.CallbackContext context)
     {
         if (!IsOwner) return;
         if (IsMovingSelector)
