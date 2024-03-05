1d3d755daecf0da86637affb3f1066f13d6a0f82
ratala <talaia.gamedev@pm.me>
2024-03-04 20:36:16 -0500

    Ajout initial (vide) du menu de construction

---
+++ Assets/Scripts/UI/MainGameUI/BuildingMenuUI.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingMenuUI : MonoBehaviour
+{
+    /// <summary>
+    /// Tours disponibles à la construction pour le niveau.
+    /// </summary>
+    [SerializeField] private TowersListSO availableTowersListSO;
+}
