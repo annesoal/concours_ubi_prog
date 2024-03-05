39b538d663d1f4a27f5449564f0df7b015981d18
ratala <talaia.gamedev@pm.me>
2024-03-04 20:33:46 -0500

    Ajout Scriptable Object BuildingMaterialSO

---
+++ Assets/Scripts/Towers/BuildingMaterialSO.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class BuildingMaterialSO : ScriptableObject
+{
+    public GameObject buildingMaterialPrefab;
+    
+    public Sprite buildingMaterialicon;
+
+    public string buildingMaterialname;
+}
