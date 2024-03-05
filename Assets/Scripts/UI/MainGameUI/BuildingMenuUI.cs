458a27d975140d5ed1d4d4e7d82bf4331c268d55
ratala <talaia.gamedev@pm.me>
2024-03-04 20:54:47 -0500

    Ajout structure affichage BuildingMenuUI

--- Assets/Scripts/UI/MainGameUI/BuildingMenuUI.cs
+++ Assets/Scripts/UI/MainGameUI/BuildingMenuUI.cs
@@ -1,11 +1,42 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Grid;
 using UnityEngine;
 
 public class BuildingMenuUI : MonoBehaviour
 {
     /// <summary>
-    /// Tours disponibles à la construction pour le niveau.
+    /// Ensemble des tours pouvant être construites par les joueurs dans le niveau.
     /// </summary>
     [SerializeField] private TowersListSO availableTowersListSO;
+
+    private void Start()
+    {
+        TowerDefenseManager.Instance.OnCurrentStateChanged += TowerDefenseManager_OnCurrentStateChanged;
+    }
+
+    private void TowerDefenseManager_OnCurrentStateChanged(object sender, TowerDefenseManager.OnCurrentStateChangedEventArgs e)
+    {
+        if (e.newValue == TowerDefenseManager.State.TacticalPause)
+        {
+            if (PlayerIsOnBuildingBlock())
+            {
+                // Show building UI (button to show it at least)
+            }
+        }
+        else
+        {
+            // Hide UI
+        }
+    }
+
+    private bool PlayerIsOnBuildingBlock()
+    {
+        Vector2Int playerPositionOnGrid = TilingGrid.LocalToGridPosition(Player.LocalInstance.transform.position);
+
+        Cell underPlayerCell = TilingGrid.grid.GetCell(playerPositionOnGrid);
+
+        return underPlayerCell.IsOf(BlockType.Buildable);
+    }
 }
