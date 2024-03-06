4bb414c77459595b5632dd5bbcd50e58d0cd65e7
Anne-Sophie Abel-Levesque <fj091076@ens.uqam.ca>
2024-03-06 11:51:08 -0500

    Rendre plus smooth le deplacement (lerp, a revoir)

--- Assets/Scripts/Ennemies/BasicEnnemy.cs
+++ Assets/Scripts/Ennemies/BasicEnnemy.cs
@@ -9,12 +9,17 @@ namespace Ennemies
     {
         private Vector2Int _nextPosition2d;
         private Vector3 _currentPosition3d;
+        private Vector3 _nextPosition3d;
         private Cell _cell;
         private EnnemyGridHelper _ennemyGridHelper;
+        public float lerpSpeed = 0.5f; 
+        
+        // Deplacements (mettre dans parent?)
         private Vector3 _avancer = new Vector3(0, 0, -1);
         private Vector3 _gauche = new Vector3(-1, 0, 0);
         private Vector3 _droite = new Vector3(1, 0, 0);
 
+        
         public BasicEnnemy()
         {
             ennemyType = EnnemyType.Basic;
@@ -29,6 +34,7 @@ namespace Ennemies
             _cell = new Cell();
             _nextPosition2d = new Vector2Int();
             _currentPosition3d = new Vector3();
+            _nextPosition3d = new Vector3();
             _cellRecorder = new CellRecorder();
             _ennemyGridHelper = new EnnemyGridHelper(currentPosition2d, _cellRecorder);
         }
@@ -63,7 +69,7 @@ namespace Ennemies
          * Avance le Helper sur la Cell en avant de l'ennemi en changeant
          * la nextPosition2d
          */
-        private void SetNextPositionAhead()
+        private void SetNextPositionAhead() //pourrait etre refactor pour prendre direction
         {
             _nextPosition2d.x = currentPosition2d.x;
             _nextPosition2d.y = currentPosition2d.y - 1; // -1 = Avancer
@@ -78,18 +84,18 @@ namespace Ennemies
         {
             ChangeEnnemyPosition3d(direction);
             ChangeEnnemyPosition2d();
-            transform.position = new Vector3(_currentPosition3d.x, _currentPosition3d.y, _currentPosition3d.z);
+            float t = Mathf.Clamp01(lerpSpeed * Time.deltaTime); // Normalisation de la vitesse
+            transform.position = Vector3.Lerp(_currentPosition3d, _nextPosition3d, t);
+            //transform.position = new Vector3(_currentPosition3d.x, _currentPosition3d.y, _currentPosition3d.z);
             _cellRecorder.AddCell(_cell);
         }
 
         /**
-         *  Modifie la position 3d de l'ennemi
+         *  Modifie la position 3d courante de l'ennemi
          */
         private void ChangeEnnemyPosition3d(Vector3 direction)
         {
             _currentPosition3d = TilingGrid.GridPositionToLocal(currentPosition2d, TilingGrid.TopOfCell + 1);
-            //_currentPosition3d.x += 0;
-            //_currentPosition3d.z += - 1;
             _currentPosition3d += direction;
         }
 
