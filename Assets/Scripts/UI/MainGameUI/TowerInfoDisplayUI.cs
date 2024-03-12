e0f9143a05ca980fa9042f077d8a6bb0dc116b45
ratala <talaia.gamedev@pm.me>
2024-03-12 12:56:50 -0400

    Ajout TowerInfoDisplayUI

---
+++ Assets/Scripts/UI/MainGameUI/TowerInfoDisplayUI.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UI;
+using UnityEngine;
+
+public class TowerInfoDisplayUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI descriptionText;
+    [SerializeField] private Transform towerVisuals;
+    
+    private void Start()
+    {
+        SingleTowerSelectUI.OnAnySingleTowerSelectUIHoveredEnter += SingleTowerSelectUI_OnAnySingleTowerSelectUIHoveredEnter;
+        SingleTowerSelectUI.OnAnySingleTowerSelectUIHoveredExit += SingleTowerSelectUI_OnAnySingleTowerSelectUIHoveredExit;
+    }
+
+    private void SingleTowerSelectUI_OnAnySingleTowerSelectUIHoveredEnter
+        (object sender, SingleTowerSelectUI.OnAnySingleTowerSelectUIHoveredEventArgs e)
+    {
+        BasicShowHide.Show(gameObject);
+        descriptionText.text = e.towerInfos.description;
+    }
+    
+    private void SingleTowerSelectUI_OnAnySingleTowerSelectUIHoveredExit
+        (object sender, SingleTowerSelectUI.OnAnySingleTowerSelectUIHoveredEventArgs e)
+    {
+        BasicShowHide.Hide(gameObject);
+    }
+
+}
