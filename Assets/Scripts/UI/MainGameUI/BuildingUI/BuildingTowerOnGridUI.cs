55b0ee6b853e2a922401b98d6214fe719a75a681
ratala <talaia.gamedev@pm.me>
2024-03-12 16:18:26 -0400

    Ajout initial BuildingTowerOnGridUI

---
+++ Assets/Scripts/UI/MainGameUI/BuildingUI/BuildingTowerOnGridUI.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingTowerOnGridUI : MonoBehaviour
+{
+    public void Show()
+    {
+        
+    }
+}
