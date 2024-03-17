9bfdf35aea09b6229980e9baf608873e9eea85b8
Nicolas Beeckmans <beeckmans.nicolas@ens.uqam.ca>
2024-03-17 18:28:36 -0400

    Introduire NetworkObjects pour ressources

--- Assets/Scripts/Grid/Ressource.cs
+++ Assets/Scripts/Grid/Ressource.cs
@@ -1,4 +1,6 @@
 using System;
+using Unity.Netcode;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace Grid
@@ -6,6 +8,7 @@ namespace Grid
     public class Ressource : MonoBehaviour
     {
         [SerializeField] private float TopOfCell = 0.72f;
+        private NetworkObject _networkObject;
 
         // Start is called before the first frame update
         void Start()
@@ -15,6 +18,13 @@ namespace Grid
                 new PlayerSelectorGridHelper(TilingGrid.LocalToGridPosition(position));
             helper.AddOnTopCell(gameObject);
             transform.position = TilingGrid.GridPositionToLocal(helper.Cell.position, 0.72f );
+            (_networkObject = this.GetComponent<NetworkObject>()).Spawn();
+        }
+
+        private void OnDestroy()
+        {
+           _networkObject.Despawn();
+           Destroy(this);
         }
     }
 }
