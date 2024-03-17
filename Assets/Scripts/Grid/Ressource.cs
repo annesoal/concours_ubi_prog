c8800e4f84d0bb1f4f05db42f0b9ba4866a10cad
Nicolas Beeckmans <beeckmans.nicolas@ens.uqam.ca>
2024-03-17 17:51:27 -0400

    Retirer debug + methode non utilisee

--- Assets/Scripts/Grid/Ressource.cs
+++ Assets/Scripts/Grid/Ressource.cs
@@ -7,7 +7,6 @@ namespace Grid
     {
         [SerializeField] private float TopOfCell = 0.72f;
 
-        public const string Name = "Resource";
         // Start is called before the first frame update
         void Start()
         {
@@ -15,21 +14,7 @@ namespace Grid
             PlayerSelectorGridHelper helper = 
                 new PlayerSelectorGridHelper(TilingGrid.LocalToGridPosition(position));
             helper.AddOnTopCell(gameObject);
-            foreach(var variable in helper.Cell.objectsOnTop)
-            {
-               Debug.Log(variable + " has been added to : " + helper.Cell.position);
-            }
             transform.position = TilingGrid.GridPositionToLocal(helper.Cell.position, 0.72f );
-        }
-        
-        public override string ToString()
-        {
-            return Name;
-        }
-
-        public void OnDestroy()
-        {
-            Debug.Log(this + " has been destroyed");
         }
     }
 }
