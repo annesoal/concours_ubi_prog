fc07bae0354f8d222dd53aa313a37880cec8433e
Anne-Sophie Abel-Levesque <fj091076@ens.uqam.ca>
2024-03-18 15:26:21 -0400

    Debuter sync client et serveur pour movement ennemi

--- Assets/Scripts/Managers/IAManager.cs
+++ Assets/Scripts/Managers/IAManager.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using DefaultNamespace;
+using Unity.Netcode;
 using UnityEngine;
 
 public class IAManager : MonoBehaviour
@@ -12,17 +13,25 @@ public class IAManager : MonoBehaviour
         Instance = this;
     }
     
-        public static void MoveEnemies(int energy)
+        public static void MoveEnemies(int totalEnergy)
         {
             Enemy enemy;
             List<GameObject> enemies = Enemy.GetEnemiesInGame();
             for(int i = enemies.Count -1; i >= 0 ; i--)
             {
                 enemy = enemies[i].GetComponent<Enemy>();
-                enemy.Move(energy);
+                enemy.Move(totalEnergy);
+
             }
             
             
+        }
+
+        // TODO 
+        [ClientRpc]
+        public static void MoveEnemyClientRpc(Enemy enemy, int totalEnergy)
+        {
+            enemy.Move(totalEnergy);
         }
 
       
