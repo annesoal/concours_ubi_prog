8fff08f1c1b7195e536bf8bc16b71e6f4efaff30
ratala <talaia.gamedev@pm.me>
2024-03-22 13:53:38 -0400

    Ajout LobbyInputManager

---
+++ Assets/Scripts/Managers/LobbyInputManager.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class LobbyInputManager : MonoBehaviour
+{
+    public static LobbyInputManager Instance { get; private set; }
+        
+    private PlayerInputActions _playerInputActions;
+    
+    private void Awake()
+    {
+        Instance = this; 
+        
+        _playerInputActions = new PlayerInputActions();
+        _playerInputActions.UI.Enable();
+        
+        _playerInputActions.UI.Select.performed += PlayerInputActionUI_OnSelectperformed;
+        _playerInputActions.UI.Cancel.performed += PlayerInputActionUI_OnCancelperformed;
+        _playerInputActions.UI.Up.performed += PlayerInputActionUI_OnUpPerformed;
+        _playerInputActions.UI.Down.performed += PlayerInputActionUI_OnDownPerformed;
+    }
+
+    public event EventHandler OnSelectUIPressed;
+    private void PlayerInputActionUI_OnSelectperformed(InputAction.CallbackContext obj)
+    {
+        OnSelectUIPressed?.Invoke(this, EventArgs.Empty);
+    }
+    
+    public event EventHandler OnCancelUIPressed;
+    private void PlayerInputActionUI_OnCancelperformed(InputAction.CallbackContext obj)
+    {
+        OnCancelUIPressed?.Invoke(this, EventArgs.Empty);
+    }
+
+    public event EventHandler OnUpUIPressed;
+    private void PlayerInputActionUI_OnUpPerformed(InputAction.CallbackContext obj)
+    {
+        OnUpUIPressed?.Invoke(this, EventArgs.Empty);
+    }
+
+    public event EventHandler OnDownUIPressed;
+    private void PlayerInputActionUI_OnDownPerformed(InputAction.CallbackContext obj)
+    {
+        OnDownUIPressed?.Invoke(this, EventArgs.Empty);
+    }
+
+}
