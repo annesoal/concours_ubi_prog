cd3a133c9a0f7cbcc9faea7e21431264144bbe78
Nicolas Beeckmans <70780656+nBeeckmans@users.noreply.github.com>
2024-03-23 15:00:29 -0700

    Retirer methodes abstraites

--- Assets/Scripts/Building/BasicTower.cs
+++ Assets/Scripts/Building/BasicTower.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Grid;
 using UnityEngine;
 using Enemies;
+using Unity.VisualScripting;
 
 /**
  * Classe d'exemple d'une tour spécifique.
@@ -15,7 +16,14 @@ public class BasicTower : BaseTower
     const float TimeToFly = 1.0f; 
     const int Radius = 2;
     const float FiringAngle = 0.4f; // En Radian !
-    [SerializeField] private GameObject _Bullet;
+
+    public BasicTower()
+    {
+        _radius = Radius; 
+        _timeToFly = TimeToFly;
+        _firingAngle = FiringAngle;
+    }
+
     public override void Build(Vector2Int positionToBuild)
     {
         towerVisuals.HidePreview();
@@ -28,46 +36,5 @@ public class BasicTower : BaseTower
     public override BuildableObjectSO GetBuildableObjectSO()
     {
         return buildableObjectSO;
-    }
-
-    public override void PlayTurn()
-    {
-        Debug.Log("Tour basique joue son tour");
-    }
-
-    protected override void SetShooter()
-    {
-        shooter = new Utils.ShootingUtility();
-        shooter.TimeToFly = TimeToFly;
-        shooter.ObjectToFire = _Bullet;
-        shooter.Angle = FiringAngle;
-    }
-
-    public override void FireOnEnemy()
-    {
-       List<Enemy> enemies = GetEnemiesInRadius(); 
-       if (!HasEnemyInRadius(enemies)) return;
-
-       Enemy enemyToFireTo = GetHighPriorityEnemy(enemies);
-       
-    }
-
-    private List<Enemy> GetEnemiesInRadius()
-    {
-        Vector2Int position = TilingGrid.LocalToGridPosition(transform.position);
-        List<Cell> cells = TilingGrid.grid.GetCellsInRadius(position, Radius);
-
-        return new List<Enemy>();
-    }
-    private Enemy GetHighPriorityEnemy(List<Enemy> enemies)
-    {
-        Enemy highPriorityEnemy = enemies[0]; 
-        for(int i = 1; i < enemies.Count; i++)
-        {
-            Enemy currentEnemy = enemies[i];
-            if (currentEnemy.distanceFromEnd < highPriorityEnemy.distanceFromEnd)
-                highPriorityEnemy = currentEnemy;            
-        }
-        return highPriorityEnemy;
     }
 }
