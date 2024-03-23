0d50804d988f4a5c33d5abc88af145e8d9fa06a3
Nicolas Beeckmans <70780656+nBeeckmans@users.noreply.github.com>
2024-03-23 13:07:09 -0700

    Commencer tire sur enemies

--- Assets/Scripts/Building/BasicTower.cs
+++ Assets/Scripts/Building/BasicTower.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Grid;
 using UnityEngine;
+using Enemies;
 
 /**
  * Classe d'exemple d'une tour spécifique.
@@ -11,6 +12,10 @@ using UnityEngine;
  */
 public class BasicTower : BaseTower
 { 
+    const float TimeToFly = 1.0f; 
+    const int Radius = 2;
+    const float FiringAngle = 0.4f; // En Radian !
+    [SerializeField] private GameObject _Bullet;
     public override void Build(Vector2Int positionToBuild)
     {
         towerVisuals.HidePreview();
@@ -28,5 +33,41 @@ public class BasicTower : BaseTower
     public override void PlayTurn()
     {
         Debug.Log("Tour basique joue son tour");
+    }
+
+    protected override void SetShooter()
+    {
+        shooter = new Utils.ShootingUtility();
+        shooter.TimeToFly = TimeToFly;
+        shooter.ObjectToFire = _Bullet;
+        shooter.Angle = FiringAngle;
+    }
+
+    public override void FireOnEnemy()
+    {
+       List<Enemy> enemies = GetEnemiesInRadius(); 
+       if (!HasEnemyInRadius(enemies)) return;
+
+       Enemy enemyToFireTo = GetHighPriorityEnemy(enemies);
+       
+    }
+
+    private List<Enemy> GetEnemiesInRadius()
+    {
+        Vector2Int position = TilingGrid.LocalToGridPosition(transform.position);
+        List<Cell> cells = TilingGrid.grid.GetCellsInRadius(position, Radius);
+
+        return new List<Enemy>();
+    }
+    private Enemy GetHighPriorityEnemy(List<Enemy> enemies)
+    {
+        Enemy highPriorityEnemy = enemies[0]; 
+        for(int i = 1; i < enemies.Count; i++)
+        {
+            Enemy currentEnemy = enemies[i];
+            if (currentEnemy.distanceFromEnd < highPriorityEnemy.distanceFromEnd)
+                highPriorityEnemy = currentEnemy;            
+        }
+        return highPriorityEnemy;
     }
 }
