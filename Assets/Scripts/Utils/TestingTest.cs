2cad9984b7c2491008d9b8672ce9e6e0e058cc56
Nicolas Beeckmans <beeckmans.nicolas@ens.uqam.ca>
2024-03-24 18:43:57 -0400

    Ajouter objet pour tester le pathfinding

---
+++ Assets/Scripts/Utils/TestingTest.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using Grid;
+using Grid.Interface;
+using UnityEngine;
+
+public class TestingTest : MonoBehaviour, ITopOfCell
+{
+    void Start()
+    {
+        TilingGrid.grid.PlaceObjectAtPositionOnGrid( this.gameObject,gameObject.transform.position);
+    }
+
+    public TypeTopOfCell GetType()
+    {
+        return TypeTopOfCell.Enemy;
+    }
+
+    public GameObject ToGameObject()
+    {
+        return this.gameObject;
+    }
+}
