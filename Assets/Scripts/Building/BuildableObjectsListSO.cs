1e724700422cce935c48a3c34a65bbd2cc503831
ratala <talaia.gamedev@pm.me>
2024-03-24 11:20:33 -0400

    Ajout liste centralisée _AllBuildableObjectsSO

--- Assets/Scripts/Building/BuildableObjectsListSO.cs
+++ Assets/Scripts/Building/BuildableObjectsListSO.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//[CreateAssetMenu()]
+[CreateAssetMenu()]
 public class BuildableObjectsListSO : ScriptableObject
 {
     [SerializeField] public List<BuildableObjectSO> list;
