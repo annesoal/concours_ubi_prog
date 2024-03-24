b4dc6ac5eb9f0cb866f029ec278b6a5f1165a3e2
ratala <talaia.gamedev@pm.me>
2024-03-24 11:17:12 -0400

    Bouger traps et tower dans dossiers respectifs

---
+++ Assets/Scripts/Building/Traps/BasicTrap.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicTrap : BaseTrap
+{
+    /// Range of "explosion" in up,down,right,left direction
+    [SerializeField] private int crossRange;
+}
