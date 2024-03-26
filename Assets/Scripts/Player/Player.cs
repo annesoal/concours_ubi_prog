8e3cc428127eb9ec8db81f70f7bbcca1254a6c84
ratala <talaia.gamedev@pm.me>
2024-03-26 14:45:29 -0400

    Ajout condition IsOwner déplacement caméra

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -126,8 +126,11 @@ public class Player : NetworkBehaviour
         {
             Debug.LogError(SPAWN_POINT_COMPONENT_ERROR);
         }
-        
-        CameraController.Instance.MoveCameraToPosition(transform.position);
+
+        if (IsOwner)
+        {
+            CameraController.Instance.MoveCameraToPosition(transform.position);
+        }
     }
     /// <summary>
     /// Demande au timer de verifier si le temps ecoule permet un nouveau deplacement
