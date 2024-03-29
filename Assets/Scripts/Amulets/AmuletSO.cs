ff64bf7b85e779884201c74a6ab247161284f5bc
Nicolas Beeckmans <beeckmans.nicolas@ens.uqam.ca>
2024-03-29 12:02:14 -0400

    Ajouter des champs

--- Assets/Scripts/Amulets/AmuletSO.cs
+++ Assets/Scripts/Amulets/AmuletSO.cs
@@ -7,10 +7,19 @@ namespace Amulets
     {
         private static int _id = 0; 
         public int ID = 0;
+        [Header("Health related issues : ")]
         public double EnemiHealthMultiplier = 1.0;
         public int PlayersHealth = 3;
+        public int EnemiHealth = 1;
+        public int TowerHealth = 5;
+        [Header("Mechanical hazards : ")]
         public float TacticalPauseTime = 20.0f;
-
+        public int numberOfTurns = 20;
+        public int playerEnergy = 10;
+        public float ResourcesSpawnRate = 0.2f;
+        public int baseTowerCost = 3;
+        [Header("Description/Assets : ")]
+        public string description = "To Be Changed/Overwritten";
         private AmuletSO()
         {
             ID = _id++;
