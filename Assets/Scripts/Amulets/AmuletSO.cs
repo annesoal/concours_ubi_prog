933553d6769f22f64b7953bfd195358aec32846f
Nicolas Beeckmans <beeckmans.nicolas@ens.uqam.ca>
2024-03-29 13:59:53 -0400

    Ajouter des champs/renommer

--- Assets/Scripts/Amulets/AmuletSO.cs
+++ Assets/Scripts/Amulets/AmuletSO.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Amulets
 {
@@ -8,16 +9,19 @@ namespace Amulets
         private static int _id = 0; 
         public int ID = 0;
         [Header("Health related issues : ")]
-        public double EnemiHealthMultiplier = 1.0;
-        public int PlayersHealth = 3;
-        public int EnemiHealth = 1;
-        public int TowerHealth = 5;
+        public double enemiHealthMultiplier = 1.0;
+        public int playersHealth = 3;
+        public int enemyBaseHealth = 1;
+        public int towerBaseHealth = 5;
+        public int enemyBaseAttack = 1;
+        public int towerBaseAttack = 1;
         [Header("Mechanical hazards : ")]
-        public float TacticalPauseTime = 20.0f;
+        public float tacticalPauseTime = 20.0f;
         public int numberOfTurns = 20;
-        public int playerEnergy = 10;
-        public float ResourcesSpawnRate = 0.2f;
-        public int baseTowerCost = 3;
+        public int energy = 10;
+        public float resourcesSpawnRate = 0.2f;
+        public int towerBaseCost = 3;
+        public int trapBaseCost = 1; 
         [Header("Description/Assets : ")]
         public string description = "To Be Changed/Overwritten";
         private AmuletSO()
