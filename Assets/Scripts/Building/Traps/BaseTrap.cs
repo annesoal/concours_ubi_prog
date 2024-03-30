c9d9c3cfab819160198ba6182fda0791b3d133b4
ratala <talaia.gamedev@pm.me>
2024-03-30 12:25:54 -0400

    Ajout comportement de base trap

--- Assets/Scripts/Building/Traps/BaseTrap.cs
+++ Assets/Scripts/Building/Traps/BaseTrap.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Enemies;
 using Grid;
 using Grid.Interface;
 using UnityEngine;
@@ -7,6 +9,12 @@ using UnityEngine;
 public abstract class BaseTrap : BuildableObject
 {
     [SerializeField] private BuildableObjectVisuals trapVisuals;
+    [SerializeField] private int damage;
+
+    private void Start()
+    {
+        Enemy.OnAnyEnemyMoved += Enemy_OnAnyEnemyMoved;
+    }
     
     public override void Build(Vector2Int positionToBuild)
     {
@@ -18,5 +26,27 @@ public abstract class BaseTrap : BuildableObject
     public override TypeTopOfCell GetType()
     {
         return TypeTopOfCell.Building;
+    }
+    
+    private void Enemy_OnAnyEnemyMoved(object sender, EventArgs e)
+    {
+        Cell trapCell = TilingGrid.grid.GetCell(TilingGrid.LocalToGridPosition(transform.position));
+
+        List<Enemy> enemiesOnTrapCell = trapCell.GetEnemies();
+
+        if (enemiesOnTrapCell.Count != 0)
+        {
+            foreach (Enemy toDamage in enemiesOnTrapCell)
+            {
+                toDamage.Damage(damage);
+            }
+        }
+        
+        Destroy(gameObject);
+    }
+
+    private void OnDestroy()
+    {
+        Enemy.OnAnyEnemyMoved -= Enemy_OnAnyEnemyMoved;
     }
 }
