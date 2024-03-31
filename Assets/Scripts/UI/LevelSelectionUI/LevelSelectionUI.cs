fa952c514d98ca1a8e19b319797dd92864ee2e3a
ratala <talaia.gamedev@pm.me>
2024-03-31 11:01:45 -0400

    Ajout serialize fields ui nécessaires

--- Assets/Scripts/UI/LevelSelectionUI/LevelSelectionUI.cs
+++ Assets/Scripts/UI/LevelSelectionUI/LevelSelectionUI.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,4 +6,30 @@ using UnityEngine;
 public class LevelSelectionUI : MonoBehaviour
 {
     [SerializeField] private LevelSelectListSO selectableLevelsListSO;
+
+    [SerializeField] private SingleLevelSelectUI singleLevelTemplateUI;
+    
+    [Header("Layouts")]
+    [SerializeField] private int maxHorizonalLayout;
+    [SerializeField] private Transform levelVerticalLayout;
+
+    private void Awake()
+    {
+        int currentHorizontalLayout = 0;
+        Transform currentVerticalLayout = Instantiate(levelVerticalLayout, transform);
+        
+        foreach (LevelSelectSO levelSO in selectableLevelsListSO.levels)
+        {
+            if (currentHorizontalLayout < maxHorizonalLayout)
+            { 
+                SingleLevelSelectUI levelSelectUI = Instantiate(singleLevelTemplateUI, currentVerticalLayout);
+            }
+            else
+            {
+                // Instantiate new vertical layout group
+            }
+            
+            currentHorizontalLayout++;
+        }
+    }
 }
--- Assets/Scripts/UI/LevelSelectionUI/SingleLevelSelectUI.cs
+++ Assets/Scripts/UI/LevelSelectionUI/SingleLevelSelectUI.cs
@@ -1,12 +1,14 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class SingleLevelSelectUI : MonoBehaviour
 {
-    // TODO levelSelectSO
+    [SerializeField] private TextMeshProUGUI nameText;
+    [SerializeField] private Transform amuletLayout;
     
     private Button _button;
     private void Awake()
