99873bd2173871325cd6a81351ddabdd88c783bd
Nicolas Beeckmans <beeckmans.nicolas@ens.uqam.ca>
2024-04-01 13:47:21 -0400

    Ajouter cleanup de bonus

--- Assets/Scripts/TowerDefenseManager.cs
+++ Assets/Scripts/TowerDefenseManager.cs
@@ -214,7 +214,11 @@ public class TowerDefenseManager : NetworkBehaviour
             Player.LocalInstance.ResetPlayer(EnergyAvailable);
             _playerReadyToPassDictionary = new Dictionary<ulong, bool>();
 
-            if (IsServer) _currentTimer.Value = tacticalPauseDuration;
+            if (IsServer)
+            {
+                _currentTimer.Value = tacticalPauseDuration;
+                CentralizedInventory.Instance.CashBonus();
+            }
         }
     }
 
@@ -356,7 +360,7 @@ public class TowerDefenseManager : NetworkBehaviour
         OnCurrentStateChanged?.Invoke(this, new OnCurrentStateChangedEventArgs
         {
             previousValue = previousValue,
-            newValue = newValue
+            newValue = newValue,
         });
     }
 
