3fdb64675ccd6a822b23b605734f6e2e23901565
ratala <talaia.gamedev@pm.me>
2024-04-01 23:25:58 -0400

    Ajout logique affichage AmuletInfoUI

--- Assets/Scripts/UI/LevelSelectionUI/AmuletInfoUI.cs
+++ Assets/Scripts/UI/LevelSelectionUI/AmuletInfoUI.cs
@@ -1,7 +1,40 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Amulets;
+using TMPro;
 using UnityEngine;
 
 public class AmuletInfoUI : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI descriptionText;
+
+    private AmuletSO _currentShowcaseAmulet;
+
+    private void Start()
+    {
+        AmuletSelectionButton.OnAmuletButtonSelected += AmuletSelectionButton_OnAmuletButtonSelected;
+        AmuletSelectionButton.OnAmuletButtonDeselected += AmuletSelectionButton_OnAmuletButtonDeselected;
+    }
+
+    private void AmuletSelectionButton_OnAmuletButtonSelected
+        (object sender, AmuletSelectionButton.OnAmuletButtonSelectedEventArgs e)
+    {
+        descriptionText.color = Color.white;
+        descriptionText.text = e.AmuletSo.description;
+
+        _currentShowcaseAmulet = e.AmuletSo;
+    }
+
+    private void AmuletSelectionButton_OnAmuletButtonDeselected
+        (object sender, AmuletSelectionButton.OnAmuletButtonDeselectedEventArgs e)
+    {
+        if (_currentShowcaseAmulet == e.AmuletSo)
+        {
+            descriptionText.color = Color.yellow;
+            descriptionText.text = "Select An Amulet To See Its Description";
+
+            _currentShowcaseAmulet = e.AmuletSo;
+        }
+    }
 }
