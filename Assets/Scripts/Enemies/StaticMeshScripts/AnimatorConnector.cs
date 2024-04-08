7feb38efbaa09623272a607c0ad88741cd4da3c5
ratala <talaia.gamedev@pm.me>
2024-04-08 16:48:19 -0400

    Ajustement destroy dans IsServer

--- Assets/Scripts/Enemies/StaticMeshScripts/AnimatorConnector.cs
+++ Assets/Scripts/Enemies/StaticMeshScripts/AnimatorConnector.cs
@@ -2,13 +2,17 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Enemies;
+using Unity.Netcode;
 using UnityEngine;
 
 public class AnimatorConnector : MonoBehaviour
 {
     public void Destroy()
     {
-        Debug.Log("inside destroy");
-        GameObject.Destroy(transform.parent.parent.gameObject);
+        if (NetworkManager.Singleton.IsServer)
+        {
+            Debug.Log("inside destroy");
+            GameObject.Destroy(transform.parent.parent.gameObject);
+        }
     }
 }
