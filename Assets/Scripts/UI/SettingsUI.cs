12d039bbddc56591741712dffa72ed0eaf1e6922
ratala <talaia.gamedev@pm.me>
2024-04-10 22:10:19 -0400

    Correction présence using erroné

--- Assets/Scripts/UI/SettingsUI.cs
+++ Assets/Scripts/UI/SettingsUI.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UI;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
