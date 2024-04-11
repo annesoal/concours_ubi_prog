7b54ef36742b1ef02a4b174b528f7cba2bb3e25e
ratala <talaia.gamedev@pm.me>
2024-04-11 10:46:32 -0400

    Réassignation bouttons SettingsUI.cs

--- Assets/Scripts/UI/SettingsUI.cs
+++ Assets/Scripts/UI/SettingsUI.cs
@@ -8,18 +8,23 @@ using UnityEngine.UI;
 
 public class SettingsUI : MonoBehaviour
 {
-    [Header("Rebinding")]
     [SerializeField] private GameObject pressToRebindUI;
     
+    [Header("Rebinding Movement")]
     [SerializeField] private Button upButton;
     [SerializeField] private Button downButton;
     [SerializeField] private Button leftButton;
     [SerializeField] private Button rightButton;
     
+    [Header("Rebinding Interactions")]
     [SerializeField] private Button selectButton;
     [SerializeField] private Button cancelButton;
     [SerializeField] private Button confirmButton;
     [SerializeField] private Button interactButton;
+    
+    [Header("Rebinding Building")]
+    [SerializeField] private Button leftCarouselButton;
+    [SerializeField] private Button rightCarouselButton;
     
     [Header("Close")]
     [SerializeField] private Button closeButton;
@@ -71,6 +76,16 @@ public class SettingsUI : MonoBehaviour
         interactButton.onClick.AddListener(() =>
         {
             CarryOutRebinding(InputManager.Binding.Interact);
+        });
+        
+        leftCarouselButton.onClick.AddListener(() =>
+        {
+            CarryOutRebinding(InputManager.Binding.ShoulderLeft);
+        });
+        
+        rightCarouselButton.onClick.AddListener(() =>
+        {
+            CarryOutRebinding(InputManager.Binding.ShoulderRight);
         });
         
         BasicShowHide.Hide(gameObject);
