d93d26994ca01ff6ad99c2fdf10053edfd48c3ea
Nicolas Beeckmans <beeckmans.nicolas@ens.uqam.ca>
2024-04-10 23:46:07 -0400

    Integrer attack

--- Assets/Scripts/Enemies/Attack/BigGuyEnemy.cs
+++ Assets/Scripts/Enemies/Attack/BigGuyEnemy.cs
@@ -1,4 +1,7 @@
-using Random = System.Random;
+using System;
+using Grid;
+using Grid.Interface;
+using UnityEngine;
 
 namespace Enemies.Attack
 {
@@ -7,6 +10,35 @@ namespace Enemies.Attack
         public BigGuyEnemy()
         {
             ennemyType = EnnemyType.BigGuy;
+        }
+
+        public override bool ChoseToAttack()
+        {
+            if (path == null || path.Count == 0)
+                return true;
+            Vector2Int positionInFront = cell.position + new Vector2Int(0, -1);
+            Cell updatedCell;
+            try
+            {
+                updatedCell = TilingGrid.grid.GetCell(positionInFront);
+            }
+            catch (ArgumentException)
+            {
+                return false;
+            }
+            if (updatedCell.HasTopOfCellOfType(TypeTopOfCell.Obstacle))
+            {
+                base.Attack(updatedCell.GetObstacle());
+                return true;
+            }
+
+            if (updatedCell.HasTopOfCellOfType(TypeTopOfCell.Obstacle))
+            {
+                base.Attack(updatedCell.GetTower());
+                return true;
+            }
+
+            return false;
         }
     }
 }
