119f148855e7a6ba3381e8e6f2f0d597e8127695
ratala <talaia.gamedev@pm.me>
2024-04-14 09:51:09 -0400

    Ajout bouttons + skybox menu principal

--- Assets/Scripts/UI/MainMenuUI/MainMenuUI.cs
+++ Assets/Scripts/UI/MainMenuUI/MainMenuUI.cs
@@ -7,14 +7,31 @@ using UnityEngine.UI;
 
 public class MainMenuUI : MonoBehaviour
 {
+    [Header("Play")]
+    [SerializeField] private Button playButton;
+    
+    [Header("Settings")]
     [SerializeField] private Button settingsButton;
     [SerializeField] private SettingsUI settingsUI;
+    
+    [Header("Quit")]
+    [SerializeField] private Button quitButton;
 
     private void Awake()
     {
+        playButton.onClick.AddListener(() =>
+        {
+            Loader.Load(Loader.Scene.LobbyScene);
+        });
+        
         settingsButton.onClick.AddListener(() =>
         {
             settingsUI.Show();
+        });
+        
+        quitButton.onClick.AddListener(() =>
+        {
+            Application.Quit();
         });
     }
 }
