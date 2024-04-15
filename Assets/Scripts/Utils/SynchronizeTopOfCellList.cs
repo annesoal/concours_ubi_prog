94bac2c2400c2cbc671911443898b3b5efb57a67
Nicolas Beeckmans <beeckmans.nicolas@ens.uqam.ca>
2024-04-14 20:10:06 -0400

    Ajouter message d'erreur

--- Assets/Scripts/Utils/SynchronizeTopOfCellList.cs
+++ Assets/Scripts/Utils/SynchronizeTopOfCellList.cs
@@ -27,6 +27,7 @@ public class SynchronizeTopOfCellList : NetworkBehaviour
         NetworkObject objectOnTopNetworkObject = objectOnTopGameObject.GetComponent<NetworkObject>();
         if (objectOnTopNetworkObject == null)
         {
+            Debug.LogError("Trying to Sync null NetWorkObject !");
             return;
         }
         
