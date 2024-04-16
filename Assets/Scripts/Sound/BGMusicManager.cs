ce71d8a666848a5b74b0e35c0ebcc6f9172bf582
Anne-Sophie Abel-Levesque <fj091076@ens.uqam.ca>
2024-04-16 19:56:25 -0400

    Enlever debug

--- Assets/Scripts/Sound/BGMusicManager.cs
+++ Assets/Scripts/Sound/BGMusicManager.cs
@@ -28,7 +28,6 @@ namespace Sound
 
         private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
         {
-            Debug.Log("La scène " + scene.name + " a été chargée.");
             PlayMusic();
         }
 
@@ -36,23 +35,18 @@ namespace Sound
         {
             if (audioSource.clip == audioClipMenu)
             {
-                Debug.Log("music : musique menu est en train de jouer");
                 if (isMenuScenes())
                     return;
-                Debug.Log("music : musique menu devrait arreter");
                 audioSource.Stop();
             }
 
             if (audioSource.clip == audioClipGame)
             {
-                Debug.Log("music : musique game est en train de jouer");
                 if (!isMenuScenes())
                     return;
-                Debug.Log("music : musique game devrait arreter");
                 audioSource.Stop();
             }
-
-            Debug.Log("music : on check pour changer de music");
+            
             StartMusic();
         }
 
@@ -60,48 +54,19 @@ namespace Sound
         {
             if (isMenuScenes())
             {
-                Debug.Log("music menu devrait commencer bientot");
-
                 audioSource.clip = audioClipMenu;
-           
-                Debug.Log("music clip  menu mis");
-                if (audioSource.enabled)
-                {
-                    Debug.Log("music audio source etait enabled");
-                }
                 audioSource.Play();
                 
                 
             }
             else
             {
-                Debug.Log("music game devrait commencer");
-                
                 audioSource.clip = audioClipGame;
-                
-                if (audioSource.enabled)
-                {
-                    Debug.Log("music audio source etait enabled");
-                }
                 audioSource.Play();
             }
         }
 
-
-        private void PlayMenuMusic()
-        {
-            Debug.Log("music menu devrait commencer bientot");
-
-            audioSource.clip = audioClipMenu;
-           
-            Debug.Log("music clip  menu mis");
-            if (audioSource.enabled)
-            {
-                Debug.Log("music audio source etait enabled");
-            }
-            audioSource.Play();
-        }
-
+        
         private bool isMenuScenes()
         {
             Debug.Log("music :  est menu scnene bool " + (SceneManager.GetActiveScene().name != "Level1" &&
